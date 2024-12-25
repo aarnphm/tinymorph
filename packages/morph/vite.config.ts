6f0b482655a8b918f3299140605dd8ff862d8ec3
nebrask <157303008+nebrask@users.noreply.github.com>
2024-12-24 19:32:30 -0500

    infra: shadcn-ui and react (#168)

--- packages/morph/vite.config.ts
+++ packages/morph/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
