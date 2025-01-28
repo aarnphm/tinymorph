25cfb1378a0fcda0e769a5fce09ac8968b0d8729
Aaron Pham <contact@aarnphm.xyz>
2025-01-27 23:29:01 -0500

    chore: initialize vLLM extensions
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

---
+++ python/exo/src/exo/__init__.py
@@ -0,0 +1,3 @@
+from .variants import register
+
+__all__ = ["register"]
---
+++ python/exo/src/exo/variants.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+def register():
+  """out-of-tree registration for intervention with SAEs.
+
+  NOTE: This method has to be extremely light and can be called multiple times.
+  """
