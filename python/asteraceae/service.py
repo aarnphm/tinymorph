2e92e7fd077263f548ad640fe4ed76078229a6dd
Aaron Pham <contact@aarnphm.xyz>
2025-02-01 10:48:51 -0500

    chore: function calling
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- python/asteraceae/service.py
+++ python/asteraceae/service.py
@@ -1,4 +1,5 @@
-import uuid, io, logging, os, traceback
+from __future__ import annotations
+import uuid, io, logging, os, traceback, functools, typing
 import bentoml, fastapi, pydantic, yaml
 
 from argparse import Namespace
@@ -11,32 +12,29 @@ logger.setLevel(logging.INFO)
 
 openai_api_app = fastapi.FastAPI()
 
-with bentoml.importing():
-  import vllm.entrypoints.openai.api_server as vllm_api_server
-
-  OPENAI_ENDPOINTS = [
-    ['/chat/completions', vllm_api_server.create_chat_completion, ['POST']],
-    ['/completions', vllm_api_server.create_completion, ['POST']],
-    ['/models', vllm_api_server.show_available_models, ['GET']],
-  ]
-  for route, endpoint, methods in OPENAI_ENDPOINTS:
-    openai_api_app.add_api_route(path=route, endpoint=endpoint, methods=methods, include_in_schema=True)
-
-
-MAX_TOKENS = 1024
+MAX_TOKENS = 4096
 MODEL_ID = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
 
 
 @bentoml.mount_asgi_app(openai_api_app, path='/v1')
 @bentoml.service(
-  name='asteraceae-instruct-service',
+  name='asteraceae-service',
   traffic={'timeout': 300, 'concurrency': 256},
-  resources={'gpu': 1, 'gpu_type': 'nvidia-tesla-a100'},
+  resources={'gpu': 1, 'gpu_type': 'nvidia-a100-80gb'},
 )
 class Engine:
   def __init__(self):
     from vllm import AsyncEngineArgs, AsyncLLMEngine
     from vllm.entrypoints.openai.api_server import init_app_state
+    import vllm.entrypoints.openai.api_server as vllm_api_server
+
+    OPENAI_ENDPOINTS = [
+      ['/chat/completions', vllm_api_server.create_chat_completion, ['POST']],
+      ['/completions', vllm_api_server.create_completion, ['POST']],
+      ['/models', vllm_api_server.show_available_models, ['GET']],
+    ]
+    for route, endpoint, methods in OPENAI_ENDPOINTS:
+      openai_api_app.add_api_route(path=route, endpoint=endpoint, methods=methods, include_in_schema=True)
 
     ENGINE_ARGS = AsyncEngineArgs(model=MODEL_ID, max_model_len=MAX_TOKENS, enable_prefix_caching=True)
     self.engine = AsyncLLMEngine.from_engine_args(ENGINE_ARGS)
@@ -56,8 +54,9 @@ class Engine:
     args.request_logger = None
     args.disable_log_stats = False
     args.return_tokens_as_token_ids = False
-    args.enable_tool_call_parser = False
     args.enable_auto_tool_choice = False
-    args.tool_call_parser = None
+    args.enable_tool_call_parser = True
+    args.tool_call_parser = "llama3_json"
+    args.enable_prompt_tokens_details = False
 
-    init_app_state(self.engine, model_config, openai_api_app.state, args)
+    vllm_api_server.init_app_state(self.engine, model_config, openai_api_app.state, args)
